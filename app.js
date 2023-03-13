d98161c92a91c06f148a905520fb1b9435443b04
suganthsugi <suganthjayanthi@gmail.com>
2023-03-13 22:59:04 +0530

    s

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: 'https://xo-tic-tac-toe-xo.netlify.app/',
+    origin: 'https://xo-tic-tac-toe-xo.netlify.app',
     credentials: true
 }));
 
